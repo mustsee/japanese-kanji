43d25d9b46ba2f5fdc941d8aa4a84624379f907c
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2023-04-06 14:36:35 +0200

    WIP add supermemo algorithm

--- pages/chapters/[id].js
+++ pages/chapters/[id].js
@@ -2,13 +2,22 @@ import Layout from "../../components/layout";
 import { getAllChaptersId, getChapterData } from "../../lib/chapters";
 import utilStyles from '../../styles/utils.module.css';
 import CardStyles from '../../styles/Card.module.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import arrayShuffle from "array-shuffle";
+import { grades, supermemo, getChapterKanji, mainState } from "../../lib/supermemo";
 
-export default function Chapter({ chapterData }) {
+export default function Chapter({ chapterData, chapterId }) {
   const [currentIndex, setIndex] = useState(0)
   const [items, setItems] = useState(chapterData.kanji)
   const [showAnswer, setShowAnswer] = useState(false)
+
+  useEffect(() => {
+    async function fetchData() {
+      const {supermemoData, deckId} = await getChapterKanji(chapterId);
+      mainState[deckId] = supermemoData
+    }
+    fetchData();
+  }, [])
 
   const handleNext = () => {
     if (currentIndex + 1 < items.length) {
@@ -33,12 +42,20 @@ export default function Chapter({ chapterData }) {
     setItems(shuffled);
   }
 
+  const handleClickOnGrade = (value) => {
+
+  }
+
+  const firestoreDeck = mainState[chapterId]
+  const firestoreScore = firestoreDeck[items[currentIndex].kanji]
+
   return (
     <Layout>
       <section className={`${utilStyles.padding1rem} ${CardStyles.header}`}>
         <span className={CardStyles.title}>{chapterData.name}</span>
         <span className={CardStyles.description}>Chapitre {chapterData.id}</span>
       </section>
+      {/* Mélanger */}
       <section className={`${utilStyles.padding1rem}`}>
       <div style={{ display: 'flex', justifyContent: 'center'}}>
           <span 
@@ -50,11 +67,13 @@ export default function Chapter({ chapterData }) {
         </div>
       </section>
       <section>
+        {/* Kanji */}
         <div style={{ padding: '3rem 0 3rem 0'}}>
           <div style={{fontSize: 4  + 'rem', textAlign: 'center'}}>
             {items[currentIndex].kanji}
           </div>
         </div>
+        {/* Suivant / Précédent */}
         <div style={{ display: 'flex', justifyContent: 'space-between', padding: '1rem'}}>
           <span 
             style={{ width: '40%', textAlign: 'center', border: '2px solid #eaeaea', padding: '0.25rem 0', backgroundColor: 'white', cursor: 'pointer'}} 
@@ -70,6 +89,33 @@ export default function Chapter({ chapterData }) {
             Suivant
           </span>
         </div>
+        {/* Grades */}
+        <div style={{ display: 'flex', justifyContent: 'space-around', padding: '1rem 0'}}>
+          {grades.map(value => {
+            return (
+              <span
+                onClick={() => handleClickOnGrade(value)}
+                key={value}
+                style={{ textAlign: 'center', border: '2px solid #eaeaea', padding: '0.25rem 1.5rem', backgroundColor: 'white', cursor: 'pointer'}} 
+              >
+                {value}
+              </span>
+            )
+          })}
+        </div>
+        {/* Scores */}
+        <div>
+          <span>
+            interval: {firestoreScore.interval}
+          </span>
+          <span>
+            repetiton: {firestoreScore.repetiton}
+          </span>
+          <span>
+            efactor: {firestoreScore.efactor}
+          </span>
+        </div>
+        {/* Meaning / elements / summary */}
         <div style={{ padding: '1rem', margin: '1rem 0', borderLeft: '2px solid #eaeaea', visibility: `${showAnswer ? 'visible' : 'hidden'}`}}>
           <div style={{ marginBottom: '0.25rem'}}>
             {items[currentIndex].meanings.FR.map((meaning, index) => {
@@ -95,6 +141,7 @@ export default function Chapter({ chapterData }) {
             {items[currentIndex].summary.FR}
           </div>
         </div>
+        {/* Afficher la réponse */}
         <div style={{ display: 'flex', justifyContent: 'center'}}>
           <span 
             style={{ width: '40%', textAlign: 'center', border: '2px solid #eaeaea', padding: '0.25rem 0', backgroundColor: 'white', cursor: 'pointer'}} 
@@ -113,6 +160,7 @@ export async function getStaticProps({ params }) {
   return {
     props: {
       chapterData,
+      chapterId: params.id
     }
   }
 }
